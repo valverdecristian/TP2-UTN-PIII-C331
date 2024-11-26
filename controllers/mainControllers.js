67e62d9d51d791b014fabb7868f11e418b95229d
valverdecristian <crizthian2010@gmail.com>
2024-11-25 22:22:38 -0300

    UPDATE: se usa la base de datos en vez del JSON para la vista

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,21 +1,22 @@
-const fs = require("fs");
-
-// Leer los archivos JSON
-const autoresJSON = JSON.parse(
-    fs.readFileSync("./data/autores.json", "utf-8")
-);
-
-const peliculasJSON = JSON.parse(
-    fs.readFileSync("./data/peliculas.json", "utf-8")
-);
+const principalModel = require("../models/principal");
+const autorModel = require("../models/autor");
 
 module.exports = {
-    index: (req, res) => {
-        res.render("main", {
-            title: "Autores y Peliculas",
-            nombre: "Autores y Peliculas UTN",
-            autores: autoresJSON,
-            peliculas: peliculasJSON,
-        });
+    index: async (req, res) => {
+        try {
+            // Consultar todos los autores y películas desde la base de datos
+            const autores = await autorModel.findAll();
+            const peliculas = await principalModel.findAll();
+
+            res.render("main", {
+                title: "Autores y Peliculas",
+                nombre: "Autores y Peliculas UTN",
+                autores: autores,
+                peliculas: peliculas,
+            });
+        } catch (error) {
+            console.error("Error al recuperar los datos:", error);
+            res.status(500).send("Error al recuperar los datos");
+        }
     },
 };
